1185bbbe5f9a3bf7ccb3fbab3d2398c578f6daad
Alex Mylle <amylle@a1-r2-p5.s19.be>
2024-12-05 14:42:44 +0100

    made test more obvious

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -18,6 +18,8 @@ Fixed b( a );
 Fixed c;
 c = b;
 std::cout << a.getRawBits() << std::endl;
+a.setRawBits(200);
+std::cout << "a is set to 200: " << a.getRawBits() << std::endl;
 std::cout << b.getRawBits() << std::endl;
 std::cout << c.getRawBits() << std::endl;
 return 0;
